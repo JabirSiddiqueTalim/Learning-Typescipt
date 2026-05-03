bfac3cb0a037ae95e5d9e5aefb62a6066173a182
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-03 21:59:08 +0600

    practice array and object ts

---
+++ src/Object.ts
@@ -0,0 +1,23 @@
+ const user:{
+  firstName :string,
+  middleName ? : string,
+  lastName :string,
+ } =
+ {
+   firstName :"jabir",
+   lastName :"talim",
+ }
+ console.log(user);
+
+ const user2:{
+  firstName :"jst",
+  middleName  : string,
+  lastName :string,
+ } =
+ {
+   firstName: "jst",
+   middleName:"siddique",
+   lastName :"talim",
+ }
+ user2.lastName ="sidd";
+ console.log(user2);
---
+++ src/array.ts
@@ -0,0 +1,6 @@
+let array =["a","cx","fg"];
+console.log(array);
+array.push("ds")
+console.log(array);
+let array2:[string,string,string]=["f","g","m"];
+let array3:[number,string]=[12,'fd']
