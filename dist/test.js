325738b531c199b48323907271c3b4459a04bb72
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-03 21:18:31 +0600

    first commit

---
+++ dist/test.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const names = 'my name is jabir';
+console.log(names);
+//# sourceMappingURL=test.js.map
---
+++ src/test.ts
@@ -0,0 +1,2 @@
+const names :string='my name is jabir';
+console.log(names)
