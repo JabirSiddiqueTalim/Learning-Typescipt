40d690b139f5fe33e46fb901b6fa44557c48588c
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-06 22:27:52 +0600

    type guard

---
+++ src/typeGuard.ts
@@ -0,0 +1,40 @@
+type parametertype=string|number;
+const Add=(num1:parametertype,num2:parametertype)=>
+{
+  if(typeof num1 ==="number" && typeof num2 === "number")
+     return num1+num2;
+  else
+  {
+    return  num1.toString()+num2.toString();
+  }
+}
+   
+Add(2,2);
+Add(3,"2");
+console.log(Add(3,"5"));
+
+type NormalUser={
+  name:string;
+
+}
+type AdminUser=
+{
+  name:string;
+  role:"Admin";
+
+}
+
+const getUser=(user :NormalUser|AdminUser)=>
+{
+  if("role" in user )
+  {
+    console.log(`${user.name } role is :${user.role}`)
+  }else
+  {
+    console.log(`${user.name } no role`)
+
+  }
+  
+
+}
+getUser({name:"jst",role:"Admin"})
