8c6bbe5c7769195ae98d56a956f9150afdc0ead4
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-05-07 15:07:40 +0600

    Statics

---
+++ src/statics.ts
@@ -0,0 +1,24 @@
+class Counter
+{
+   static count=0;
+  increment()
+  {
+    return(Counter.count=Counter.count+1)
+
+  }
+}
+const B1=new Counter();
+console.log(B1.increment());
+console.log(B1.increment());
+
+console.log(B1.increment());
+console.log(B1.increment());
+console.log(B1.increment());
+console.log(B1.increment());
+const B=new Counter();
+console.log(B.increment());
+console.log(B.increment());
+console.log(B.increment());
+console.log(B.increment());
+console.log(B.increment());
+console.log(B.increment());
